d883f512a7b96858612e338d4be2c9e66c9f33de
deepu1067 <dshahadat10@gmail.com>
2025-01-21 00:13:59 +0600

    edited the dataset_structure file

--- Dataset Structure.py
+++ Dataset Structure.py
@@ -1,8 +1,8 @@
 import json
 
 # File paths
-input_file_path = "C:\\Users\\adnan\\Desktop\\filtered_adverse.json"  # Replace with your raw data JSON file path
-output_file_path = "adverse_reactions_dataset.json"
+input_file_path = "dataset/filtered_adverse.json"  # Replace with your raw data JSON file path
+output_file_path = "dataset/adverse_reactions_dataset.json"
 
 # Read raw data from JSON file
 with open(input_file_path, "r") as file:
@@ -24,8 +24,15 @@ for entry in raw_data:
         "response_text": response_text
     })
 
+cleaned_data = []
+for entry in structured_data:
+    if entry["response_text"].startswith("6 ADVERSE REACTIONS"):
+        entry["response_text"] = entry["response_text"].replace("6 ADVERSE REACTIONS", "ADVERSE REACTIONS", 1)
+    cleaned_data.append(entry)
+
+
 # Save structured dataset to a JSON file
 with open(output_file_path, "w") as json_file:
-    json.dump(structured_data, json_file, indent=4)
+    json.dump(cleaned_data, json_file, indent=4)
 
 print(f"Structured data saved to {output_file_path}")
